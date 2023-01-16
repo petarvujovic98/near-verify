27f4f1a0e686f286f3c7834445a6bcf52349d780
Petar Vujović <petarvujovic98@gmail.com>
2023-01-16 16:28:00 +0100

    feat: Add navbar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { WalletSelectorContextProvider } from "../context/WalletSelectorContext"
 
 import "../styles/globals.css";
 import "@near-wallet-selector/modal-ui/styles.css";
+import Header from "../components/header";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <WalletSelectorContextProvider>
       <div className="h-screen bg-gray-300 dark:bg-gray-700">
+        <Header />
         <Component {...pageProps} />
       </div>
     </WalletSelectorContextProvider>
